2b968bdafbcaaab6892e05ccb7e05c6684a1f27e
Luke <rio8562@icloud.com>
2022-05-26 18:02:49 -0400

    need to learn how to debug properly for future tests

--- MadLibs.py
+++ MadLibs.py
@@ -1,25 +1,33 @@
 """ Mad Libs Generator
 ----------------------------------------
 """
+
+# okay so my plan is to debug and make sure i understand how all of this stuff works
+
 # Loop back to this point once code finishes
 loop = 1
-while (loop < 10):
-# All the questions that the program asks the user
+
+#so i went to start to debug my program but i forgot that i need to put inputs for it to work
+while loop < 10:
+    # All the questions that the program asks the user
+
     noun = input("Choose a noun: ")
     p_noun = input("Choose a plural noun: ")
     noun2 = input("Choose a noun: ")
     place = input("Name a place: ")
     adjective = input("Choose an adjective (Describing word): ")
     noun3 = input("Choose a noun: ")
-# Displays the story based on the users input
-    print ("------------------------------------------")
-    print ("Be kind to your",noun,"- footed", p_noun)
-    print ("For a duck may be somebody's", noun2,",")
-    print ("Be kind to your",p_noun,"in",place)
-    print ("Where the weather is always",adjective,".")
-    print ()
-    print ("You may think that is this the",noun3,",")
-    print ("Well it is.")
-    print ("------------------------------------------")
-# Loop back to "loop = 1"
+
+    # Displays the story based on the users input
+    print("------------------------------------------")
+    print("Be kind to your", noun, "- footed", p_noun)
+    print("For a duck may be somebody's", noun2, ",")
+    print("Be kind to your", p_noun, "in", place)
+    print("Where the weather is always", adjective, ".")
+    print()
+    print("You may think that is this the", noun3, ",")
+    print("Well it is.")
+    print("------------------------------------------")
+
+    # Loop back to "loop = 1"
     loop = loop + 1
